9cd26855476910c9fb6876b22ca6c5e1e14610c2
Rafael de Luca <deluca1712@gmail.com>
2023-07-11 11:42:19 -0300

    Project created

---
+++ src/main/java/com/rafaeldeluca/dslist/games/DslistGamesApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.dslist.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistGamesApplication.class, args);
+	}
+
+}
