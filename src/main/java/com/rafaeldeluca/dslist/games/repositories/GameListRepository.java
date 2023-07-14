8b4e2689d86f85cfcb8b106a570e1ae546e40a76
Rafael de Luca <deluca1712@gmail.com>
2023-07-14 16:18:45 -0300

    Function to reposition the list games when move them on the frontend

--- src/main/java/com/rafaeldeluca/dslist/games/repositories/GameListRepository.java
+++ src/main/java/com/rafaeldeluca/dslist/games/repositories/GameListRepository.java
@@ -1,9 +1,16 @@
 package com.rafaeldeluca.dslist.games.repositories;
 import com.rafaeldeluca.dslist.games.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
-
+    // query to update the position
+    @Modifying // it is not a SELECT query
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging " +
+            "SET position = :newPosition " +
+            "WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 
 }
--- src/main/java/com/rafaeldeluca/dslist/games/services/GameListService.java
+++ src/main/java/com/rafaeldeluca/dslist/games/services/GameListService.java
@@ -3,7 +3,9 @@ package com.rafaeldeluca.dslist.games.services;
 
 import com.rafaeldeluca.dslist.games.dto.GameListDTO;
 import com.rafaeldeluca.dslist.games.entities.GameList;
+import com.rafaeldeluca.dslist.games.projections.GameMinProjection;
 import com.rafaeldeluca.dslist.games.repositories.GameListRepository;
+import com.rafaeldeluca.dslist.games.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,13 +20,39 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
-    @Transactional(readOnly = false)
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
     public List<GameListDTO> findAllGameList () {
         List<GameList> listGameList = new ArrayList<GameList>();
         listGameList = gameListRepository.findAll();
         return listGameList.stream().map( g -> new GameListDTO(g)).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = false)
+    public void moveList(Long listId, int sourceIndex, int destinationIndex) {
+        // first get the list, than change the position of the games
+        List<GameMinProjection> gameList = gameRepository.searchGamesByList(listId); // list on the memory
+        GameMinProjection sourceGame = gameList.remove(sourceIndex);
+        gameList.add(destinationIndex, sourceGame);
 
+        int lower = sourceIndex;
+        if (sourceIndex < destinationIndex) {
+            lower = sourceIndex;
+        } else {
+            lower = destinationIndex;
+        }
 
+        int  higher= sourceIndex;
+        if(sourceIndex < destinationIndex) {
+            higher = destinationIndex;
+        } else {
+            higher = sourceIndex;
+        }
+
+        for(int newPosition = lower; newPosition <= higher; newPosition++) {
+            gameListRepository.updateBelongingPosition(listId,gameList.get(newPosition).getId(), newPosition);
+        }
+    }
 }
